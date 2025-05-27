d3472633eadcff9c4fa9c175116412f597a8b795
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-05-28 01:49:48 +0600

    Using Object As State In useReducer

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,26 +1,19 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [state, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   function reducer(state, action) {
     if (action.type == "increment") {
-      return state + action.payload;
+      return { ...state, count: state.count + action.payload };
     }
     if (action.type == "decrement") {
-      return state - action.payload;
+      return { ...state, count: state.count - action.payload };
     }
   }
   return (
     <div>
       <div>
-        <h1>Count is: {count}</h1>
-        <button onClick={(e) => setCount(count + 1)}>Increment</button>
-        <button onClick={(e) => setCount(count - 1)}>Decrement</button>
-      </div>
-
-      <div>
-        <h1>Use reducer count: {state}</h1>
+        <h1>Use reducer count: {state.count}</h1>
         <button onClick={(e) => dispatch({ type: "increment", payload: 1 })}>
           Increment state
         </button>
