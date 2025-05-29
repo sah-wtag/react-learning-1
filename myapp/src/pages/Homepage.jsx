1d555f9a9a8b80b5bd8a706d8688be0eef1b02c8
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-05-29 18:15:39 +0600

    useNavigate Hook In React

--- myapp/src/pages/Homepage.jsx
+++ myapp/src/pages/Homepage.jsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Homepage() {
+  const navigate = useNavigate();
+  const [username, setUserName] = useState("");
+  function handleClick() {
+    navigate("/dashboard/profile", { state: { username } });
+  }
   return (
     <div>
       <h1>This is a Homepage</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={handleClick}>Go to profile</button>
     </div>
   );
 }
--- myapp/src/pages/Profile.jsx
+++ myapp/src/pages/Profile.jsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 export default function Profile() {
+  const location = useLocation();
+  const { username } = location.state || {};
+
   return (
     <div>
-      <h1>This is profile page</h1>
+      <h1>This is profile page for {username}</h1>
     </div>
   );
 }
