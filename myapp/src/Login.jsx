a1895714bcdbe22cabbe0a1be8a58b7d0f231b3d
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-06-01 15:47:04 +0600

    Passing Complex State To Reducer

--- myapp/src/Login.jsx
+++ myapp/src/Login.jsx
@@ -3,10 +3,9 @@ import { userContext } from "./context/UserContext";
 
 export default function Login() {
   const [value, setValue] = useState("");
-  const { user, dispatch } = useContext(userContext);
+  const { state, dispatch } = useContext(userContext);
   function handleSubmit(e) {
     e.preventDefault();
-    // setUser(value);
     dispatch({ type: "login", payload: value });
   }
   return (
--- myapp/src/context/UserContext.jsx
+++ myapp/src/context/UserContext.jsx
@@ -2,18 +2,17 @@ import { createContext, useReducer, useState } from "react";
 const userContext = createContext();
 
 function UserProvider({ children }) {
-  // const [user, setUser] = useState("Guest");
   function userReducer(state, action) {
     if (action.type == "login") {
-      return action.payload;
+      return { user: action.payload };
     }
     if (action.type == "logout") {
-      return action.payload;
+      return { user: action.payload };
     }
   }
-  const [user, dispatch] = useReducer(userReducer, "");
+  const [state, dispatch] = useReducer(userReducer, { user: null });
   return (
-    <userContext.Provider value={{ user, dispatch }}>
+    <userContext.Provider value={{ ...state, dispatch }}>
       {children}
     </userContext.Provider>
   );
